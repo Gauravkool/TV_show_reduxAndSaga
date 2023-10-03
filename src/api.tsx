e13e6df102291bad2ae17ca4f4348269a02a618d
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-10-03 23:36:14 +0530

    use async await method instead of then method

--- src/api.tsx
+++ src/api.tsx
@@ -2,12 +2,21 @@ import axios from "axios";
 import { Show } from "./models/Show";
 
 const BASE_URL = "https://api.tvmaze.com/";
-export const searchShows = (keyword: string) => {
+
+export const searchShows = async (keyword: string) => {
+  const res = await axios.get<{ show: Show }[]>(
+    BASE_URL + "search/shows?q=" + keyword
+  );
+  return res.data.map((item) => item.show);
+};
+
+export const searchShows2 = (keyword: string) => {
   return axios
     .get<{ show: Show }[]>(BASE_URL + "search/shows?q=" + keyword)
     .then((res) => res.data.map((item) => item.show));
 };
 
-export const loadShowDetail = (showId: number) => {
-  return axios.get(BASE_URL + "shows/" + showId).then((res) => res.data);
+export const loadShowDetail = async (showId: number) => {
+  const res = await axios.get(BASE_URL + "shows/" + showId);
+  return res.data;
 };
